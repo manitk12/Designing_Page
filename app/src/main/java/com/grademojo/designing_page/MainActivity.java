930de757aa3508adc93c68ccd2f30d1824fb3f13
manit <ermanit007@gmail.com>
2017-08-03 18:32:43 +0530

    border set in cardview in top ,left , bottom , right

---
+++ app/src/main/java/com/grademojo/designing_page/MainActivity.java
@@ -0,0 +1,13 @@
+package com.grademojo.designing_page;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.logn_page);
+    }
+}
